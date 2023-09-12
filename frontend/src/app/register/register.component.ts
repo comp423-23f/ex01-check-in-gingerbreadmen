49ce0f342485ca6e2ef31e1621ba75ec31c88e04
Andrew Knotts <knottsan2010@gmail.com>
2023-09-12 01:09:43 +0000

    finished story C

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -10,26 +10,46 @@ export class RegisterComponent {
 
   firstNameMessage = ''
 
+  
   user = {
     firstName: '',
     lastName: '',
     pid: ''
   };
 
+  userT = {
+    firstName: '',
+    lastName: '',
+    pid: ''
+  };
+
+ 
+
   showThankYouMessage = false;
 
   constructor(private userService: UserService) {}
 
   onSubmit() {
+
+    this.user = {
+      firstName: this.userT.firstName,
+      lastName: this.userT.lastName,
+      pid: this.userT.pid
+    };
+
     this.userService.addUser(this.user)
     console.log('User Registration Data:', this.firstNameMessage);
     console.log('User Array:', this.userService.getUsers);
     
     this.firstNameMessage = this.user.firstName;
-    this.user.firstName = '';
-    this.user.lastName = '';
-    this.user.pid = '';
+    
     this.showThankYouMessage = true;
+
+    this.userT = {
+      firstName: '',
+      lastName: '',
+      pid: ''
+    };
 
   }
 
